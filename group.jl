c7c314254fc63cdf4b45acc8a818b7d09e0b6470
Con <conor.houghton@bristol.ac.uk>
2024-05-26 10:00:44 +0100

    group written not tested

--- group.jl
+++ group.jl
@@ -1,29 +1,34 @@
-#in this version members in clans, fit clans breed among themselves
+#in this version agents are put in groups
+#speed of breeding depends on how long to be put in a fit group
 
 using Random
 using Distributions
 
-mutable struct Clan
+mutable struct Group
     members::Vector{Vector{Bool}}
     n::Int
     fit::Bool
+    t::Int #time the Group becomes fit
 end
 
-function makeClan(members::Vector{Vector{Bool}})
-    clan=Clan(members,length(members[1]),false)
-    clan.fit=checkFit(clan)
-    clan
+function makeGroup(members::Vector{Vector{Bool}},t,tN)
+    group=Group(members,length(members[1]),false,tN+1)
+    group.fit=checkFit(group)
+    if group.fit
+        group.t=t
+    end
+    group
 end
 
-function makeClan(n,clanN)
-    members=[makeAgent(n) for _ in 1:clanN]
-    makeClan(members)
+function makeGroup(n,groupN)
+    members=[makeAgent(n) for _ in 1:groupN]
+    makeGroup(members)
 end
 
 
-function makeClan(n,clanN,p)
-    members=[makeAgent(n,p) for _ in 1:clanN]
-    makeClan(members)
+function makeGroup(n,groupN,p)
+    members=[makeAgent(n,p) for _ in 1:groupN]
+    makeGroup(members)
 end
 
 
@@ -54,15 +59,15 @@ function breed(agent1::Vector{Bool},agent2::Vector{Bool},mutation::Real)
     
 end
 
-function checkFit(clan::Clan)
-    reduce((x,y) -> x & y, reduce((x, y) -> x .| y, clan.members))
+function checkFit(group::Group)
+    reduce((x,y) -> x & y, reduce((x, y) -> x .| y, group.members))
 end
 
-function selectClan(clans::Vector{Clan}, lamb::Float64)
+function selectGroup(groups::Vector{Group}, lamb::Float64)
 
-    p = ones(Float64, length(clans))
-    for i in 1:length(clans)
-        if clans[i].fit
+    p = ones(Float64, length(groups))
+    for i in 1:length(groups)
+        if groups[i].fit
             p[i] *= lamb
         end
     end
@@ -74,50 +79,62 @@ function selectClan(clans::Vector{Clan}, lamb::Float64)
     
     selected_index = rand(dist)
     
-    return clans[selected_index]
+    return groups[selected_index]
 end
 
-function breedClan(clan::Clan,mutation::Real)
-    members=Vector{Vector{Bool}}()
 
-    for _ in 1:length(clan.members)
-        p1=rand(1:length(clan.members))
-        p2=p1
-        while p1==p2
-            p2=rand(1:length(clan.members))
-        end
-        push!(members,breed(clan.members[p1],clan.members[p2],mutation))
+function selectAgent(groups::Vector{Group}, p::Vector{Float64})
+    
+    total = sum(p)
+    p /= total
+    
+    dist = Categorical(p)
+    
+    selected_index = rand(dist)
+    
+    return rand(groups[selected_index])
+end
+
+
+
+function groupP(groups::Vector{Group}, value::Function)
+    p=Vector{Float64}(undef,length(groups))
+
+    for i in 1:length(groups)
+        p[i]=value(groups[i].g)
     end
 
-    makeClan(members)
+    return p
 end
 
-function success(clans::Vector{Clan})
+    
+function breedGroup(group::Group,mutation::Real)
+    members=Vector{Vector{Bool}}()
+
+    for _ in 1:length(group.members)
+        p1=rand(1:length(group.members))
+        p2=p1
+        while p1==p2
+            p2=rand(1:length(group.members))
+        end
+        push!(members,breed(group.members[p1],group.members[p2],mutation))
+    end
+
+    makeGroup(members)
+end
+
+function success(groups::Vector{Group})
     totalTrues = 0
     totalAgents = 0
     
-    for clan in clans
-        for member in clan.members
+    for group in groups
+        for member in group.members
             totalTrues += count(x-> x==true, member)
             totalAgents += 1
         end
     end
     
     return totalTrues / totalAgents
-end
-
-function successClan(clans::Vector{Clan})
-    totalTrues = 0
-    totalClans = 0
-    
-    for clan in clans
-        if clan.fit
-            totalTrues += 1
-        end
-        totalClans += 1
-    end
-    
-    return totalTrues / totalClans
 end
 
 n=20
@@ -127,26 +144,68 @@ mutation=0.00
 e1=4
 e2=10-e1
 
-clanN=2^e1
+groupN=2^e1
 worldN=2^e2
 
-g=40
+gN=40
+tN=20
 
-p=0.25
+pOn=0.25
 
-world=[makeClan(n,clanN,p) for _ in 1:worldN]
+value(t)=tN+2-t
 
-println(success(world)," ",successClan(world))
+agents=[makeAgent(n,pOn) for _ in 1:2^{e1+e2)]
 
-for _ in 1:g
-    for i in 1:length(world)
-        clan=selectClan(world,lamb)
-        world[i]=breedClan(clan,mutation)
-    end
-    println(success(world)," ",successClan(world))
+world=Vector{Group}()
 
+for i in 1:worldN
+    push!(world,makeGroup(agents[i*groupN:(1+1)*groupN-1],1))
 end
 
+println(success(world))
+
+for g in 1:gN
+    global world
+    for t in 1:tN
+        agents=Vector{Agent}()
+        newWorld=Vector{Group}()
+        for w in 1:length(world)
+            if !world[w].fit
+                append!(agents,world[w].members)
+            else
+                push!(newWorld,world[w])
+            end
+        end
+        
+        shuffle!(agents)
+        newWorldN=length(agents)/groupN
+        
+        for i in 1:newWorldN
+            push!(newWorld,makeGroup(agents[i*groupN:(1+1)*groupN-1],t,tN))
+        end
+
+        world=copy(newWorld)
+
+        println(success(world))
+        
+    end
+
+    pSelect=groupP(groups,value)
+
+    agents=Vector{Agents}(undef,worldN*groupN)
+    
+    for a in 1:worldN*groupN
+        selectAgent(groups,pSelect)
+        agent[a]=breed(selectAgent(groups,pSelect),selectAgent(groups,pSelect))
+    end
+
+
+    for i in 1:worldN
+        push!(world,makeGroup(agents[i*groupN:(1+1)*groupN-1],1))
+    end
+    
+
+end
 
 
     
